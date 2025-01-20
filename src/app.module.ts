f21825d56ee4516fd3c96485c6ce140ab497b871
snakepatch53 <snakepatch53@gmail.com>
2025-01-20 18:30:02 -0500

    - Instale y configuré typeORM - Instale y configuré el Linter

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: process.env.DB_HOST,
+            port: parseInt(process.env.DB_PORT),
+            username: process.env.DB_USER,
+            password: process.env.DB_PASS,
+            database: process.env.DB_NAME,
+            entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+            synchronize: process.env.PROD === 'true' ? false : true,
+        }),
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
