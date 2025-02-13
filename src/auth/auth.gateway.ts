88533a4d43908e6dc0d084907d4426c81efcd193
snakepatch53 <snakepatch53@gmail.com>
2025-02-12 20:07:17 -0500

    Changes to be committed: 	modified:   src/auth/auth.gateway.ts

--- src/auth/auth.gateway.ts
+++ src/auth/auth.gateway.ts
@@ -18,14 +18,14 @@ export class AuthGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
     // Al conectarse un socket, no pedimos token. Lo dejamos libre.
     handleConnection(@ConnectedSocket() client: Socket) {
-        console.log(`Client connected: ${client.id}`);
+        // console.log(`Client connected: ${client.id}`);
         // Guardar referencia
         this.socketsMap.set(client.id, client);
     }
 
     // Al desconectarse
     handleDisconnect(@ConnectedSocket() client: Socket) {
-        console.log(`Client disconnected: ${client.id}`);
+        // console.log(`Client disconnected: ${client.id}`);
         this.socketsMap.delete(client.id);
     }
 
